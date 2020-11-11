6c111deea55c6eaa75064b8207a0a7f291c30f66
Jason Aravanis <j.aravanis@icloud.com>
2020-11-11 21:23:24 +1100

    Cleared up prior changes, haven't added makeMove functionality yet

--- index copy.js
+++ index copy.js
@@ -1,6 +1,12 @@
 const gameBoardDiv = document.getElementById("gameBoard");
 
 const Gameboard = (() => {
+
+    init = () => {
+        addListeners()
+        render()
+    }
+
     // What does a gameboard do? Store information, and update appearance for users
     let gameboard = [
         [null, null, null],
@@ -8,27 +14,14 @@ const Gameboard = (() => {
         [null, null, null],
     ]
 
-    // tiles array to hold DOM elements
-    const tiles = [];
-    for (let row = 0; row < gameboard.length; row++) {
-        let rowHolder = [];
-        for (let col = 0; col < gameboard.length; col++) {
-            let tile = gameBoardDiv.querySelector(`#tile-${row}-${col}`)
-            tile.setAttribute("data-row", row);
-            tile.setAttribute("data-col", col);
-            rowHolder.push(tile)
-        }
-        tiles.push(rowHolder)
-    }
-
-    // Render the DOM elements - Public
+    // Render/Update the DOM elements - Public
     render = () => {
-        for (let row = 0; row < gameboard.length; row++) {
-            for (let col = 0; col < gameboard.length; col++) {
+        for (let row = 0; row < Gameboard.gameboard.length; row++) {
+            for (let col = 0; col < Gameboard.gameboard.length; col++) {
                 if (gameboard[row][col] == true) {
-                    tiles[row][col].textContent = "X"
+                    Gameboard.tiles[row][col].textContent = "X"
                 } else if (gameboard[row][col] == false) {
-                    tiles[row][col].textContent = "O"
+                    Gameboard.tiles[row][col].textContent = "O"
                 } else {
                     Gameboard.tiles[row][col].textContent = "."
                 }
@@ -37,7 +30,23 @@ const Gameboard = (() => {
         console.log("Gameboard has been rendered")
     }
 
-    return { gameboard, render, tiles }
+    // Event listeners
+    const tiles = [];
+    addListeners = () => {
+        for (let row = 0; row < Gameboard.gameboard.length; row++) {
+            let rowHolder = [];
+            for (let col = 0; col < Gameboard.gameboard.length; col++) {
+                let tile = gameBoardDiv.querySelector(`#tile-${row}-${col}`)
+                tile.setAttribute("data-row", row);
+                tile.setAttribute("data-col", col);
+                tile.addEventListener("click", Game.makeMove(tile))
+                rowHolder.push(tile)
+            }
+            tiles.push(rowHolder)
+        }
+    }
+
+    return { gameboard, render, addListeners, tiles, init }
 })()
 
 
@@ -61,6 +70,23 @@ const Game = (() => {
         } else {
             return playerTwo
         }
+    }
+
+    makeMove = (tile) => {
+        _updateTile = (tile, player) => {
+            console.log(`player = ${player.name}`)
+            let row = tile.dataset.row
+            let col = tile.dataset.col
+            Gameboard.gameboard[row][col] = (player == Game.playerOne) ? true : false;
+        }
+
+        let player = Game.getActivePlayer();
+        _updateTile(tile, player)
+        Gameboard.render()
+        Game.checkVictory(player)
+        Game.toggleActivePlayer()
+
+
     }
 
     toggleActivePlayer = () => {
@@ -89,9 +115,7 @@ const Game = (() => {
         // Check rows for victory
         for (let i = 0; i < victoryMatrix.length; i++) {
             let row = victoryMatrix[i];
-            if (_checkWin(row, player)) {
-                endGame(player)
-            }
+            _checkWin(row, player);
         }
         // Check columns for victory
         for (let col = 0; col < 3; col++) {
@@ -99,55 +123,22 @@ const Game = (() => {
             for (let row = 0; row < victoryMatrix.length; row++) {
                 colArray.push(victoryMatrix[row][col])
             }
-            if (_checkWin(colArray, player)) {
-                endGame(player);
-            }
+            _checkWin(colArray, player)
         }
         // Check diagonals for victory
         let diagArray = [];
         for (let index = 0; index < victoryMatrix.length; index++) {
             diagArray.push(victoryMatrix[index][index]);
         }
-        if (_checkWin(diagArray, player)) {
-            endGame(player)
-        }
+        _checkWin(diagArray, player);
         diagArray = [];
         for (let index = 0; index < victoryMatrix.length; index++) {
             diagArray.push(victoryMatrix[index][2 - index])
         }
-        if (_checkWin(diagArray, player)) {
-            endGame(player)
-        };
+        _checkWin(diagArray, player);
     }
 
-    endGame = (player) => {
-        console.log("endgame function here");
-    }
-
-    return { getActivePlayer, toggleActivePlayer, playerOne, playerTwo }
+    return { getActivePlayer, toggleActivePlayer, playerOne, playerTwo, makeMove }
 })()
 
-Gameboard.render();
-
-
-
-
-// tile.addEventListener("click", Game.makeMove(tile))
-
-
-// makeMove = (tile) => {
-//     _updateTile = (tile, player) => {
-//         console.log(`player = ${player.name}`)
-//         let row = tile.dataset.row
-//         let col = tile.dataset.col
-//         Gameboard.gameboard[row][col] = (player == Game.playerOne) ? true : false;
-//     }
-
-//     let player = Game.getActivePlayer();
-//     _updateTile(tile, player)
-//     Gameboard.render()
-//     Game.checkVictory(player)
-//     Game.toggleActivePlayer()
-
-
-// }
+Gameboard.init();
